6dec13aa9b68f061e563201e096ec46c10578331
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-08-10 15:52:27 -0400

    BE added join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_192944) do
+ActiveRecord::Schema.define(version: 2020_08_10_194018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2020_08_10_192944) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments_posts", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "comment_id", null: false
   end
 
   create_table "posts", force: :cascade do |t|
