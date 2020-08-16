2a48792c0b29137f3dedb201005c1571c10f2c22
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-08-16 14:18:31 -0400

    user CRUD created

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,7 +7,7 @@ class AuthenticationController < ApplicationController
     if @user.authenticate(login_params[:password]) #authenticate method provided by Bcrypt and 'has_secure_password'
       token = encode({id: @user.id})
       render json: {
-        user: @user.attributes.except("password_digest"),
+        user: @user.attributes.except('password_digest'),
         token: token
         }, status: :ok
     else
@@ -17,6 +17,10 @@ class AuthenticationController < ApplicationController
   
   # GET /auth/verify
   def verify
-    render json: @current_user.attributes.except("password_digest"), status: :ok
+    render json: @current_user.attributes.except('password_digest'), status: :ok
+  end
+  private
+  def login_params
+    params.require(:authentication).permit(:username, :password)
   end
 end
